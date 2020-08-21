5b359c304c828ecfbe10610beb9fff0bb8fd1eaa
jakebacker <jbacker42@gmail.com>
2020-08-21 13:39:17 -0400

    Add Simulation skeleton code

--- CovidStatsConfig.cs
+++ CovidStatsConfig.cs
@@ -2,10 +2,9 @@ namespace CovidSimulator
 {
     public class CovidStatsConfig
     {
-        // This is likely a very large overestimate
         // https://covidactnow.org/us/ma?s=863169
         public static readonly double R0 = 2.35;
-        public static readonly double AverageLength = 8;
+        public static readonly double AverageLength = 10;
         public static readonly double InfectionRate = R0 / AverageLength; // Infections per day
         
         // https://www.healio.com/news/primary-care/20200609/rtpcr-yields-high-falsenegative-rates-in-early-sarscov2-infection
--- Simulation.cs
+++ Simulation.cs
@@ -9,10 +9,39 @@ namespace CovidSimulator
     public class Simulation
     {
         private People _graph;
+
+        private int _currentInfections = 0;
+        private int _totalInfections = 0;
+        private int _maxInfections = 0;
         
         public Simulation(People graph)
         {
             _graph = graph;
+
+            _currentInfections = FindInfections();
+            _totalInfections = _currentInfections;
+            _maxInfections = _currentInfections;
+        }
+
+        /**
+         * <summary>
+         * Search through the entire graph to find all infected people
+         * </summary>
+         *
+         * <returns>The number of people currently infected</returns>
+         */
+        private int FindInfections()
+        {
+            int infections = 0;
+            for (var p = 0; p < _graph.NumPeople(); p++)
+            {
+                if (_graph.GetPerson(p).IsInfected())
+                {
+                    infections++;
+                }
+            }
+
+            return infections;
         }
 
         /**
@@ -24,8 +53,67 @@ namespace CovidSimulator
          */
         public bool RunSimulation()
         {
+            while (_currentInfections > 0)
+            {
+                // This is definitely not the fastest way, but it makes it more modular and such.
+                ProcQuarantine();
+                ProcTest();
+                ProcInfectedTasks();
+            }
 
             return true;
         }
+
+        /**
+         * <summary>
+         * Check everyone to see if they need to quarantine. If they do, remove all connections.
+         * </summary>
+         */
+        private void ProcQuarantine()
+        {
+            
+        }
+
+        /**
+         * <summary>
+         * Test everyone who is supposed to be tested and set their quarantine flag if they test positive.
+         * </summary>
+         */
+        private void ProcTest()
+        {
+            
+        }
+
+        /**
+         * <summary>
+         * Wrapper for <see cref="ProcInfectOthers"/> and <see cref="ProcUpdateInfection"/>
+         * </summary>
+         */
+        private void ProcInfectedTasks()
+        {
+            ProcInfectOthers();
+            ProcUpdateInfection();
+        }
+
+        /**
+         * <summary>
+         * For everyone who is infected, try and infect all connections based on weights.
+         * </summary>
+         */
+        private void ProcInfectOthers()
+        {
+            
+        }
+
+        /**
+         * <summary>
+         * For everyone who is infected, update their infection day count. If it is at the limit, make them recover.
+         * </summary>
+         */
+        private void ProcUpdateInfection()
+        {
+            
+        }
+
     }
 }
